11c0b8290a630a111f45050352cae8e1ce4e1150
Hayden Crain <haydencrain@optusnet.com.au>
2019-06-12 19:37:56 +1000

    allow videos to be blurred, by placing them in the '/video' dir

--- privacy/S3FD.pytorch/generate.py
+++ privacy/S3FD.pytorch/generate.py
@@ -89,7 +89,7 @@ def resize_image(cv2Img):
     return resizedImg, tensor_scale
 
 
-def detect(net, img, thresh):
+def detect_and_blur(net, img, thresh):
     image, scale = resize_image(img)
     image_tensor = create_tensor_module(image)
 
@@ -100,32 +100,67 @@ def detect(net, img, thresh):
     return result_image
 
 
-def detect_from_path(net, save_dir, img_path, thresh):
-    print('detecting: {}'.format(img_path))
+def generate_image(net, save_dir, img_path, thresh):
+    print('Protecting Image {}...'.format(img_path))
     img = cv2.imread(img_path, cv2.IMREAD_COLOR)
     t1 = time.time()
-    result_image = detect(net, img, thresh)
-    cv2.imwrite(os.path.join(save_dir, os.path.basename(img_path)), result_image)
+    result_image = detect_and_blur(net, img, thresh)
+    cv2.imwrite(get_new_save_path(save_dir, img_path), result_image)
     t2 = time.time()
     print('done. timer: {}'.format(t2 - t1))
 
 
+def generate_video(net, vid_path, save_dir, thresh):
+    print('Protecting Video {}...'.format(vid_path))
+    t1 = time.time()
+    output_path = get_new_save_path(save_dir, vid_path)
+    print(output_path)
+    # create video capture and get video info
+    vidcap = cv2.VideoCapture(vid_path)
+    fps = vidcap.get(cv2.CAP_PROP_FPS)
+    size = (int(vidcap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(vidcap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+    # create video writer
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    vidwrite = cv2.VideoWriter(output_path, fourcc, fps, size)
+
+    # Read initial frame
+    count = 1
+    success, frame = vidcap.read()
+    while success:
+        result_frame = detect_and_blur(net, frame, thresh)
+        # Write video
+        vidwrite.write(result_frame)
+
+        # Read video
+        success, frame = vidcap.read()
+        count += 1
+
+    vidcap.release()
+    vidwrite.release()
+
+    t2 = time.time()
+    print('done. timer: {}'.format(t2 - t1))
+
+
+def get_new_save_path(save_dir, current_path):
+    return os.path.join(save_dir, os.path.basename(current_path))
+
+
 def generate_images(net, save_dir, thresh):
-    img_path = './img'
-    img_list = [os.path.join(img_path, x)
-                for x in os.listdir(img_path) if x.endswith('jpg')]
-
-    for path in img_list:
-        detect_from_path(net, save_dir, path, thresh)
-
-
-def generate_video(net, vid_path, thresh):
-    frames = video_to_frames(vid_path)
-    for index, frame in enumerate(frames):
-        # frames[index] = detect(net, frame, thresh)
-        print(index)
+    img_dir = './img'
+    for filename in os.listdir(img_dir):
+        if filename.lower().endswith('jpg'):
+            img_path = os.path.join(img_dir, filename)
+            generate_image(net, save_dir, img_path, thresh)
 
 
+def generate_videos(net, save_dir, thresh):
+    video_dir = './video'
+    for filename in os.listdir(video_dir):
+        if filename.lower().endswith('mp4'):
+            video_path = os.path.join(video_dir, filename)
+            generate_video(net, video_path, save_dir, thresh)
+    
 
 def build_net(model): 
     net = build_s3fd('test', cfg.NUM_CLASSES)
@@ -141,30 +176,3 @@ def build_net(model):
     net.load_state_dict(state_dict)
     net.eval()
     return net
-
-
-def video_to_frames(vid_path, output_path):
-    # create video capture and get video info
-    vidcap, fps, size = get_video_capture(vid_path)
-    # create video writer
-    fourcc = cv2.VideoWriter_fourcc(*'MJPG')
-    vidwrite = cv2.VideoWriter(output_path, fourcc, fps, size)
-   
-    frames = []
-    success = True
-    while success:
-        # Read video
-        success, frame = vidcap.read()
-        # Write video
-        vidwrite.write(frame)
-
-    vidcap.release()
-    vidwrite.release()
-    return frames
-
-
-def get_video_capture(vid_path):
-    vidcap = cv2.VideoCapture(vid_path)
-    fps = vidcap.get(cv2.CAP_PROP_FPS)
-    size = (int(vidcap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(vidcap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
-    return vidcap, fps, size
